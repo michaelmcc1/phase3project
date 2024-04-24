574d71e0c5b4c76bc94f45fd30637e9553cc2366
Michael McCutcheon <michael.mccutcheon1@yahoo.com>
2024-04-23 20:18:27 -0400

    completed stage02 add getCustomer

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
- 
+
 const express = require('express');
 const path = require('path');  // for handling file paths
+const da = require("./data-access");
 
 const app = express();
 const port = process.env.PORT || 4000;  // use env var or default to 4000
 
 // Set the static directory to serve files from
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get("/customers", async (req, res) => {
+    const cust = await da.getCustomers();
+    res.send(cust); 
+   });
+
+   
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
